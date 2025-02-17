c619e01790bc66d4154b8ea124611a08c7f6b333
Ambrose MM <brosegm@gmail.com>
2025-02-17 17:19:52 -0500

    Initial commit

---
+++ travel_recommendation.js
@@ -0,0 +1,7 @@
+        function searchFunction() {
+            let query = document.getElementById("search").value;
+            alert("Searching for: " + query);
+        }
+        function resetFunction() {
+            document.getElementById("search").value = "";
+        }
